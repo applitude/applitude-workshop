e19a83d9c01fdd015e3b5ec9c52cfd4b385e4eb6
Espen Næss <espennaess@Espens-MacBook-Pro.local>
2016-09-28 20:05:25 +0200

    Oppgåver

---
+++ Oppgaver.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+// Oppgave 1a
+
+Endre slik at applikasjonen også laster inn en ny svadasetning når applikasjonen åpnes
+
+Hint: endre viewDidLoad metoden
+
+// Oppgave 1b
+
+Endre tekstfargen og tekststørrelsen på svadageneratoren
+
+Hint: Se i XCode sin GUI eller se på klassene UIColor og UIFont og egenskapene til en label
+
+// Oppgave 2a
+
+Endre bakgrunnsfargen til applikasjonen
+
+Hint: Dette kan gjøres grafisk eller i koden
+
+// Oppgave 2b
+
+Endre fra oppgave 2a slik at bakgrunnsfargen endres til nye farger etterhvert som brukeren henter nye svadasetninger
+
+Hint: Se på UIColor klassen og arc4random funksjonen
+
+// Oppgave 3
+
+Endre applikasjonen slik at den husker hvilken setning (og evt bakgrunnsfarge) når applikasjonen terminerer
+Det vil si at når brukeren åpner applikasjonen ETTER AT DEN ER TERMINERT så vil den ha samme setning og bakrgunnsfarge som da den terminerte
+
+Hint: Se på klassen NSUserDefaults
+
+// Oppgave 4 (kanskje noe vrien for nybegynnere)
+
+Endre applikasjonen slik at når brukeren går ut og inn av applikasjonen genereres en ny setning
+
+Hint: Ha en variabel i AppDelegate som refererer til ViewControlleren.
+
